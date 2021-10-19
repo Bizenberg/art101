9fe4ee2b7638f30513d108dfdf690af8da639ca7
Benji Izenberg <bizenber@ucsc.edu>
2021-10-18 18:24:21 -0700

    Lab 5
    Lab 5 created, images uploaded, css and HTML files written. First attempt at JS completed!

---
+++ lab5/js/lab.js
@@ -0,0 +1,23 @@
+/*
+ * Author: Benjamin Izenberg <bizenberg@ucsc.edu>
+ * Created: 10.18.21
+ * License: Public Domain
+ * File: art101/lab5/js/lab.js
+ * Link: bizenberg.github.io/art101
+ */
+
+ //Define Variables
+ make = "Subaru";
+ model = "Outback";
+ color = "Emerald";
+ year = "1998";
+
+ //calculate
+ age = 2021 - year;
+
+ //output
+ document.writeln("Make: " + make + "<br>");
+ document.writeln("Model: " + model + "<br>");
+ document.writeln("Color: " + color + "<br>");
+ document.writeln("Year: " + year + "<br>");
+ document.writeln("Age: " + age + " years<br>");
